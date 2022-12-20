ad1f331d85107f23df63e0dd8a51982cf64cdf26
RaymondChowYinHong <raymondchow@student.usm.my>
2022-12-20 19:06:07 +0800

    Create main.hpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 //create food function
 //create funtion to store patient disease, charges, name and ID
 #include <iostream>
+#include "main.hpp"
 using namespace std;
-
-void displaymenu();
-double Calc_Total_Charge(double , double , double , double , double );
-double Calc_medcine_charge(double , int&);
-double Calc_surgery_charge(double , int&);
-double Calc_service_charge(double , int&);
-double Calc_hospitalstay_charge(double, int&);
-double Calc_fooddrink_charge(double , int&);
 
 int main()
 {
---
+++ main.hpp
@@ -0,0 +1,7 @@
+void displaymenu();
+double Calc_Total_Charge(double , double , double , double , double );
+double Calc_medcine_charge(double , int&);
+double Calc_surgery_charge(double , int&);
+double Calc_service_charge(double , int&);
+double Calc_hospitalstay_charge(double, int&);
+double Calc_fooddrink_charge(double , int&);
